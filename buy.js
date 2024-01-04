e334b03291e4a21125e810becfd84d92f84219ad
MRAman126 <125187210+MRAman126@users.noreply.github.com>
2024-01-04 13:27:30 +0530

    new

---
+++ buy.js
@@ -0,0 +1,9 @@
+/* function Sendmail(){
+    let parms = {
+        name : document.getElementById("name").Value,
+        email : document.getElementById("email").Value,
+        phone : document.getElementById("phone").Value,
+        address : document.getElementById("address").Value,
+    }
+    email.send("service_6n97qmi","template_ht6cqdq",parms).then(alert("your order is booked"))
+} */
